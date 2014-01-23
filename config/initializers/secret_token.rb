301ef252c0b1fc5348dcc0f4087d01681e277bcb
Andrew Shipman <andy.shipman@yahoo.com>
2014-01-23 12:25:23 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThereWillBeBlood::Application.config.secret_key_base = 'dc57970365a35b65fe54bbfab2c09722ca9625a94dded3d68d6979add24bcea299a22ea515e4e3821f435c7e8e5d016b022a2443e50809592fbd57c0e16b0bf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThereWillBeBlood::Application.config.session_store :cookie_store, key: '_thereWillBeBlood_session'
