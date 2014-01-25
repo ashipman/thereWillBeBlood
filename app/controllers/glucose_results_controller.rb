4b45c3b2e4cbdb0412c4f72a102df3c5ba1ef4d9
Andrew Shipman <andy.shipman@yahoo.com>
2014-01-24 16:10:18 -0800

    Add deletion functionality to glucose results, including fetching only 'in_use' records.

--- app/controllers/glucose_results_controller.rb
+++ app/controllers/glucose_results_controller.rb
@@ -1,39 +1,45 @@
 class GlucoseResultsController < ApplicationController
 
   def new
-    #Nothing to do here - just show me the damn form!
+    # Nothing to do here - just show me the damn form!
   end
 
   def create
-    #Assign a new instance of the "GlucoseResult" class to an @instanceVariable, passing in the relevant attributes.
-    @glucose_result = GlucoseResult.new(glucose_result_params)
-    #Save the instance (attempt to create a db record, result will determine if we were successful.)
+    # Assign a new instance of the "GlucoseResult" class to an @instanceVariable, passing in the relevant attributes.
+		@glucose_result = GlucoseResult.new( glucose_result_params )
+		@glucose_result.in_use = 1
+    # Save the instance (attempt to create a db record, result will determine if we were successful.)
     @glucose_result.save
-    #Redirct to the 'show' action.
+    # Redirct to the 'show' action.
     redirect_to @glucose_result
   end
 
   def show
-    #Update the @instanceVariable with the item we want to show by finding it in the db using the :id identifier.
-    @glucose_result = GlucoseResult.find(params[:id])
+    # Update the @instanceVariable with the item we want to show by finding it in the db using the :id identifier.
+    @glucose_result = GlucoseResult.find( params[:id] )
+		if @glucose_result.in_use=0
+			@glucose_result=nil
+		else
+			@glucose_result
+		end
   end
 
   def index
-    #Assign all existing results to the @instancevariable.
-    @glucose_results = GlucoseResult.all
+    # Assign all existing results (that have the 'in_use' property set to true) to the @instancevariable.
+    @glucose_results = GlucoseResult.where( "in_use= 1" )
   end
 
   def edit
-    #Find the item based on the provided :id.
-    @glucose_result = GlucoseResult.find(params[:id])
+    # Find the item based on the provided :id.
+    @glucose_result = GlucoseResult.find( params[:id] )
   end
 
   def update
-    #Try to update the record.
-    @glucose_result = GlucoseResult.find(params[:id])
-    #If the update works, go to the 'show' screen for that record, otherwise re-show this screen (allowing validation
+    # Try to update the record.
+    @glucose_result = GlucoseResult.find( params[:id] )
+    # If the update works, go to the 'show' screen for that record, otherwise re-show this screen (allowing validation
     # messages be shown.)
-    if @glucose_result.update(glucose_result_params)
+    if @glucose_result.update( glucose_result_params )
       redirect_to @glucose_result
     else
       render 'edit'
@@ -41,18 +47,20 @@ class GlucoseResultsController < ApplicationController
   end
 
   def destroy
-    #Find the item based on the provided :id.
-    @glucose_result = GlucoseResult.find(params[:id])
-    #Destroy it!!!
-    @glucose_result.destroy
-    #Redirect the user to an appropriate location.
+    # Find the item based on the provided :id.
+    @glucose_result = GlucoseResult.find( params[:id] )
+    # Simulate destroying it by changing the 'in_use' flag.
+		@glucose_result.in_use = 0
+		@glucose_result.save
+		# Destroy it!!!
+		# @glucose_result.destroy
+    # Redirect the user to an appropriate location.
     redirect_to glucose_results_path
   end
 
   private
     def glucose_result_params
-      #"strong_parameters" requires that we define what parameters we want to accept in the controller.
-      params[:in_use] = true
-			params.require(:glucose_result).permit(:user_id, :date_time_stamp, :result, :device, :notes, :in_use)
+      # "strong_parameters" requires that we define what parameters we want to accept in the controller.
+			params.require( :glucose_result ) .permit( :user_id, :date_time_stamp, :result, :device, :notes )
     end
 end
